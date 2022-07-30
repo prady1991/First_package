e00e2b9de9181bde11d5ea146a90af80e87de003
Siva Tekuru <prady1991@gmail.com>
2022-07-30 15:45:28 +0100

    Initial Push

--- SiPr/lib.py
+++ SiPr/lib.py
@@ -1,5 +1,5 @@
 def try_me(a,b):
     return a+2*b
 
-if __name__=='main':
-    print('you dont have to be a genius to evaluate this',try_me(2,3))
+if __name__== '__main__':
+    print('you dont have to be a genius to evaluate this function a+2*b',try_me(2,3))
